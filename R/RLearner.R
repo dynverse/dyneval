2a87c277fefe70ceac7060dfcd40016996bc18c0
Robrecht Cannoodt <brechtus@gmail.com>
2017-04-06 15:20:48 +0200

    Added sample scorpius ti_learner and ginhoux dataset

--- R/RLearner.R
+++ R/RLearner.R
@@ -1,3 +1,5 @@
+#' TI R Learner
+#'
 #' @import mlr
 #' @export
 #' @rdname RLearner
@@ -8,12 +10,22 @@ RLearnerTI <- function(cl, package, par.set, par.vals = list(), properties = cha
   )
 }
 
-make_task_data <- function(expression, gold_structure, gold_cells) {
-  list(
+read_ti_task_data <- function(dataset_name) {
+  readRDS(paste0("data/", dataset_name, ".rds"))
+}
+
+make_ti_task_data <- function(ti_type, name, expression, state_network, state_percentages, sample_info = NULL, feature_info = NULL) {
+  l <- list(
+    type = "ti",
+    ti_type = ti_type,
+    name = name,
     expression = expression,
-    structure = gold_structure,
-    cells = gold_cells
+    state_network = state_network,
+    state_percentages = state_percentages,
+    sample_info = sample_info,
+    feature_info = feature_info
   )
+  l
 }
 
 
@@ -23,7 +35,7 @@ get_task_data <- function(.task, .subset) {
   .task
 }
 
-wrap_ti_output <- function(structure, cells) {
-  l <- list(structure = structure, cells = cells)
+wrap_ti_output <- function(state_network, state_percentages) {
+  l <- list(state_network = state_network, state_percentages = state_percentages)
   l
 }
