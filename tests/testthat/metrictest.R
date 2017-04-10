3476cbbb8d14182df7a016d4019e353339d79c61
Robrecht Cannoodt <brechtus@gmail.com>
2017-04-10 15:47:16 +0200

    Support adding phantom edges in joining edges

--- tests/testthat/metrictest.R
+++ tests/testthat/metrictest.R
@@ -1,52 +1,83 @@
 library(tidyverse)
 library(dyneval)
 
+my_read_ods <- function(path = NULL, sheet = 1, col_names = TRUE, col_types = NULL, na = "", skip = 0, formula_as_formula = FALSE, range = NULL) {
+  res <- readODS:::parse_ods_to_sheets(path)
+  ods_ns <- res[[2]]
+  sheets <- res[[1]]
+  target_sheet <- readODS:::select_sheet(sheets, ods_ns = ods_ns, which_sheet = sheet)
+  cell_values <- readODS:::parse_rows(target_sheet, ods_ns, formula_as_formula = formula_as_formula, skip = skip)
+  parsed_df <- readODS:::to_data_frame(cell_values = cell_values, header = col_names, na = na)
+  if (length(col_types) == 1 && is.null(col_types)) {
+    raw_sheet <- readr::type_convert(df = parsed_df)
+  } else if (length(col_types) == 1 && is.na(col_types)) {
+    raw_sheet <- parsed_df
+  } else {
+    raw_sheet <- readr::type_convert(df = parsed_df, col_types = col_types)
+  }
+  if (!is.null(range)) {
+    res <- readODS:::select_range(raw_sheet, range)
+  } else {
+    res <- raw_sheet
+  }
+  return(res)
+}
+
+read_trajectory_ods <- function(name) {
+  cat("Reading ", name, "\n", sep = "")
+  filename <- paste0(data_folder, "/", name, "/gold.ods")
+
+  structure_coltypes <- cols_only(from = col_character(), to = col_character(), length = col_double())
+  cells_coltypes <- cols(cellid = col_character(), .default = col_double())
+
+  # read structure tab
+  structure <- my_read_ods(
+    filename,
+    sheet = "structure",
+    col_types = structure_coltypes
+  ) %>% mutate(
+    from = ifelse(grepl("^[0-9]*$", from), paste0("milestone_", from), from),
+    to = ifelse(grepl("^[0-9]*$", to), paste0("milestone_", to), to)
+  )
+
+  milestones <- sort(unique(c(structure$from, structure$to)))
+
+  # read cells tab
+  cells <- my_read_ods(
+    filename,
+    sheet = "cells",
+    col_types = cells_coltypes
+  ) %>%
+    mutate(id = paste0("cell_", cellid)) %>%
+    select(id, one_of(milestones))
+
+  # fill in NAs
+  for (milestone in milestones) {
+    cells[,milestone] <- ifelse(is.na(cells[,milestone]), 0, cells[,milestone])
+  }
+
+  dyneval::wrap_ti_prediction(
+    ti_type = "*",
+    name = name,
+    state_names = milestones,
+    state_network = structure,
+    state_percentages = cells,
+    task_id = NULL
+  )
+}
+
 data_folder <- "tests/testthat/data_metrictest"
 files <- list.files(data_folder)
-
-trajectory_data <- lapply(files, function(name) {
-  cat("Reading ", name, "\n", sep = "")
-  gold_path <- paste0(data_folder, "/", name, "/gold.ods")
-  traj <- dyneval:::read_ods_trajectory(gold_path)
-  dimensionality_reduce_trajectory(name, traj)
-})
+trajectory_data <- lapply(files, read_trajectory_ods)
+trajectoryplot_data <- lapply(trajectory_data, plotLearnerData.ti.default)
 
 all_trajs <- list(
-  milestones = bind_rows(trajectory_data %>% map(~ .$milestones)),
-  lines = bind_rows(trajectory_data %>% map(~ .$lines)),
-  cells = bind_rows(trajectory_data %>% map(~ .$cells))
+  space_states = bind_rows(trajectoryplot_data %>% map(~ .$space_states)),
+  space_lines = bind_rows(trajectoryplot_data %>% map(~ .$space_lines)),
+  space_samples = bind_rows(trajectoryplot_data %>% map(~ .$space_samples))
 )
+class(all_trajs) <- "dyneval::ti_dimred_wrapper"
 
-# make_trajectory_plot(trajectory_data[[7]])
-# make_trajectory_plot(trajectory_data[[6]])
-
-# pdf("alltestplots.pdf", 16, 12)
-make_trajectory_plot(all_trajs) + facet_wrap(~name, ncol = 5)
-# dev.off()
+plotLearner.ti.default(all_trajs) + facet_wrap(~name)
 
 
-gold_path <- paste0(data_folder, "/1a_linear/gold.ods")
-pred_path <- paste0(data_folder, "/1a_linear/prediction_good.ods")
-traj <- dyneval:::read_ods_trajectory(gold_path)
-pred_traj <- dyneval:::read_ods_trajectory(pred_path)
-dimensionality_reduce_trajectory(name, traj)
-
-# # test renaming
-# traj <- list(
-#   structure = data.frame(from = "x", to = "y", length = 1, stringsAsFactors = F),
-#   cells = data_frame(x = seq(0, 1, by = .1), y = 1 - x)
-# )
-# drtraj <- dimensionality_reduce_trajectory("test", traj)
-# make_trajectory_plot(drtraj)
-#
-# # test more than 12 milestones
-# traj <- list(
-#   structure = data.frame(from = letters[-26], to = letters[-1], length = 1, stringsAsFactors = F),
-#   cells = local({
-#     d <- diag(length(letters))
-#     colnames(d) <- letters
-#     as.data.frame(d)
-#   })
-# )
-# drtraj <- dimensionality_reduce_trajectory("test", traj)
-# make_trajectory_plot(drtraj)
