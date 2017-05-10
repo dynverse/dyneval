66cfee39768459b984bbba1ae321bfe8d6761d74
Robrecht Cannoodt <brechtus@gmail.com>
2017-05-10 14:46:20 +0200

    Worked on creating a mlr trainer running

--- R/ti_scorpius.R
+++ R/ti_scorpius.R
@@ -6,8 +6,8 @@ makeRLearner.ti.scorpius <- function() {
     cl = "ti.scorpius",
     package = c("SCORPIUS"),
     par.set = makeParamSet(
-      makeIntegerLearnerParam(id = "num_dimensions", lower = 1L, default = 3L),
-      makeIntegerLearnerParam(id = "num_clusters", lower = 2L, default = 4L)
+      makeIntegerLearnerParam(id = "num_dimensions", lower = 2L, default = 3L, upper = 20L),
+      makeIntegerLearnerParam(id = "num_clusters", lower = 2L, default = 4L, upper = 20L)
     ),
     properties = c("numerics", "dimred", "dimred_traj", "pseudotime"),
     name = "SCORPIUS",
@@ -18,11 +18,10 @@ makeRLearner.ti.scorpius <- function() {
 #' @importFrom SCORPIUS correlation.distance reduce.dimensionality infer.trajectory
 #' @importFrom tibble data_frame
 #' @export
-trainLearner.ti.scorpius <- function(.task, .subset, num_dimensions, num_clusters) {
-  # subsetting will not work yet, but the function is already provided
-  data <- get_task_data(.task, .subset)
+trainLearner.ti.scorpius <- function(.learner, .task, .subset, num_dimensions = 3, num_clusters = 4) {
+  data <- getTaskData(.task, .subset)
 
-  expression <- data[["expression"]]
+  expression <- as.matrix(data)
   dist <- SCORPIUS::correlation.distance(expression)
   space <- SCORPIUS::reduce.dimensionality(dist, ndim = num_dimensions)
   traj <- SCORPIUS::infer.trajectory(space, k = num_clusters)
@@ -62,8 +61,8 @@ plotLearner.ti.scorpius <- function(ti_predictions) {
     scale_color_viridis()
 }
 
-# registerS3method("makeRLearner", "ti.scorpius", makeRLearner.ti.scorpius)
-# registerS3method("trainLearner", "ti.scorpius", trainLearner.ti.scorpius)
-# registerS3method("plotLearner", "ti.scorpius", plotLearner.ti.scorpius)
-
-
+#' @export
+predictLearner.ti.scorpius <- function(.learner, .model, .newdata, ...) {
+  # .model$pseudotime
+  NULL
+}
--- scripts/robrecht/training_test.R
+++ scripts/robrecht/training_test.R
@@ -1,3 +1,4 @@
+library(cowplot)
 library(mlr)
 library(tidyverse)
 library(dyneval)
@@ -11,7 +12,21 @@ tasks <- lapply(seq_len(nrow(datasets_info)), function(dataset_num) {
   dataset_id <- datasets_info$id[[dataset_num]]
   dataset <- dyngen::load_dataset(dataset_id, contents = dyngen::contents_dataset(experiment = F))
 
-  task <- with(dataset, dyneval::wrap_ti_task_data(
+  expression <- as.data.frame(log2(dataset$counts+1))
+  gold <- list(
+    state_names = dataset$gs$milestone_names,
+    state_network = dataset$gs$milestone_net,
+    state_percentages = dataset$gs$milestone_percentages %>% slice(match(rownames(dataset$counts), id))
+  )
+
+  makeTITask(id = paste0(dataset$model$modulenetname, "_", dataset$info$id), data = expression, gold = gold)
+})
+
+task_wrapped <- lapply(seq_len(nrow(datasets_info)), function(dataset_num) {
+  dataset_id <- datasets_info$id[[dataset_num]]
+  dataset <- dyngen::load_dataset(dataset_id, contents = dyngen::contents_dataset(experiment = F))
+
+  with(dataset, dyneval::wrap_ti_task_data(
     ti_type = model$modulenetname,
     name = info$id,
     expression = log2(counts+1),
@@ -21,13 +36,43 @@ tasks <- lapply(seq_len(nrow(datasets_info)), function(dataset_num) {
   ))
 })
 
-
+# try with scorpius and the first task
 learner <- makeLearner("ti.scorpius")
-task <- makeTITask(data = as.data.frame(tasks[[1]]$expression))
-mod <- train(learner, task)
+task <- tasks[[1]]
+task_wrap <- task_wrapped[[1]]
 
-print(mod)
-## use random forest to classify iris data
-task = makeClassifTask(data = iris, target = "Species")
-learner = makeLearner("classif.rpart", minsplit = 7, predict.type = "prob")
-mod = train(learner, task, subset = training.set)
+
+# train using default params
+model <- train(learner, task)
+performance(pred = NULL, measures = list(corank_auc, corank_max, geodesic_cor), task = task, model = model)
+
+# train the parameters in a grid
+ctrl <- makeTuneControlGrid(resolution = 5L)
+rdesc <- makeResampleDesc("CV", iters = 5L)
+res <- tuneParams(learner, task, rdesc, par.set = learner$par.set, control = ctrl)
+
+# train a new model using these params
+par_values <- res$x
+model <- train(makeLearner("ti.scorpius", par.vals = par_values), task)
+
+g0 <- plotLearner.ti.default(task_wrap)
+g1 <- plotLearner.ti.default(model$learner.model)
+g2 <- plotLearner.ti.scorpius(model$learner.model)
+g3 <- plotLearner.ti.combined(task_wrap, model$learner.model)
+cowplot::plot_grid(g0, g1, g2, g3)
+
+
+
+
+# benchmarking?
+learner <- makeLearner("ti.scorpius")
+rdesc <- makeResampleDesc("CV", iters = 2L)
+bm <- benchmark(learner, tasks, rdesc)
+
+
+ctrl = makeTuneControlIrace(maxExperiments = 200)
+ctrl = makeTuneControlGrid(resolution = 10L)
+# ctrl = makeTuneControlMBO()
+rdesc = makeResampleDesc("CV", iters = 2L)
+# rdesc = makeResampleDesc("Bootstrap", iters = 2L)
+res = tuneParams(learner, task, rdesc, par.set = learner$par.set, control = ctrl)
