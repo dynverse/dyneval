1e6c5934a7811005e4ed3106c7e073ee5e6a1f09
Wouter Saelens <wouter.saelens@gmail.com>
2017-05-11 13:56:55 +0200

    added branch based evaluation

--- R/metrics.R
+++ R/metrics.R
@@ -170,3 +170,37 @@ compute_coranking <- function(gold_dist, pred_dist) {
     summary = summary
   )
 }
+
+#' Convert percentages to a unique edge assignment
+cal_branch_assignment = function(percentages, network) {
+  branch_assignment <- apply(percentages, 1, function(x) {
+    positives = names(which(x > 0))
+    if(length(intersect(positives, network$from)) > 0) {
+      intersect(positives, network$from)[[1]]
+    } else {
+      network %>% filter(to==positives[[1]]) %>% .$from %>% .[[1]]
+    }
+  }) %>% factor() %>% as.numeric()
+}
+
+#' Compute the F1rr based on two set of assignment labels
+F1rr = function(labels1, labels2) {
+  overlaps = map(unique(labels1), function(label1) {
+    map_dbl(unique(labels2), function(label2) {
+      sum((labels1 == label1) & (labels2 == label2)) / sum((labels1 == label1) | (labels2 == label2))
+    })
+  }) %>% invoke(cbind, .)
+  print(overlaps)
+  2/(1/mean(apply(overlaps, 1, max)) + 1/mean(apply(overlaps, 2, max)))
+}
+
+#' Compute the F1rr score for overlap of branches
+#'
+#' Can currently not handle multiple linear edges without branching, which should be merged in the future
+#'
+#' @export
+score_prediction_F1rr = function(task, pred_output) {
+  branch_assignment_true = cal_branch_assignment(task$state_percentages %>% select(-id), task$state_network)
+  branch_assignment_observed = cal_branch_assignment(pred_output$state_percentages %>% select(-id), pred_output$state_network)
+  F1rr(branch_assignment_true, branch_assignment_observed)
+}
