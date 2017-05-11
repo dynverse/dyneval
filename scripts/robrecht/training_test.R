323b6db4b7f4cba93f49d7cd520e694248b08f83
Robrecht Cannoodt <brechtus@gmail.com>
2017-05-11 11:29:32 +0200

    working training with mbo

--- scripts/robrecht/training_test.R
+++ scripts/robrecht/training_test.R
@@ -23,41 +23,40 @@ task_wrapped <- lapply(seq_len(nrow(datasets_info)), function(dataset_num) {
 })
 task_tib <- to_tibble(task_wrapped)
 
-task <- makeTITask(id = "dyngen", data = task_tib)
+task <- makeTITask(id = "dyngen", data = task_tib[c(1,6,9,20),])
 
 learner <- makeLearner("ti.scorpius")
-learner <- makeLearner("ti.monocleDDRtree")
-learner <- makeLearner("ti.tscan")
+# learner <- makeLearner("ti.monocleDDRtree")
+# learner <- makeLearner("ti.tscan")
 
-# train using default params
-model <- train(learner, task)
-pred <- predict(model, task)
-
-performance(pred, measures = list(corank_auc, corank_max, geodesic_cor))
+# # train using default params
+# model <- train(learner, task)
+# pred <- predict(model, task)
+#
+# performance(pred, measures = list(corank_auc, corank_max, geodesic_cor))
 
 # train the parameters in a grid
-ctrl <- makeTuneControlGrid(resolution = 2L)
+# ctrl <- makeTuneControlGrid(resolution = 2L)
+ctrl <- makeTuneControlMBO()
 rdesc <- makeResampleDesc("CV", iters = 2L)
-res <- tuneParams(learner, task, rdesc, par.set = learner$par.set, control = ctrl)
+par <- makeParamSet(
+  makeIntegerParam(id = "num_dimensions", lower = 2L, default = 3L, upper = 20L),
+  makeIntegerParam(id = "num_clusters", lower = 2L, default = 4L, upper = 20L),
+  makeDiscreteParam(id = "distance_method", default = "spearman", values = c("spearman", "pearson", "kendall"))
+)
 
-# # train a new model using these params
-# par_values <- res$x
-# model <- train(makeLearner("ti.scorpius", par.vals = par_values), task)
-#
-# g0 <- plotLearner.ti.default(task_wrap)
-# g1 <- plotLearner.ti.default(model$learner.model)
-# g2 <- plotLearner.ti.scorpius(model$learner.model)
-# g3 <- plotLearner.ti.combined(task_wrap, model$learner.model)
-# cowplot::plot_grid(g0, g1, g2, g3)
-#
-#
-#
-#
-# ctrl = makeTuneControlIrace(maxExperiments = 200)
-# ctrl = makeTuneControlGrid(resolution = 10L)
-# # ctrl = makeTuneControlMBO()
-# rdesc = makeResampleDesc("CV", iters = 2L)
-# # rdesc = makeResampleDesc("Bootstrap", iters = 2L)
-# res = tuneParams(learner, task, rdesc, par.set = learner$par.set, control = ctrl)
-#
-#
+configureMlr(on.learner.warning = "quiet", show.learner.output = FALSE)
+
+library(parallelMap)
+# parallelStartMulticore(cpus = 8, show.info = TRUE)
+res <- tuneParams(learner = learner, task = task, resampling = rdesc, measures = corank_max, par.set = par, control = ctrl)
+# parallelStop()
+
+
+
+
+
+
+
+
+
