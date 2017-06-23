4f7b25f5be057ab13a98a9b618b6795241e93f76
Helena Todorov <helenatodorov@MacBook-Pro-de-Helena.local>
2017-06-23 12:07:13 +0200

    DPT and waterfall scripts

--- scripts/helena/waterfall_dyneval.R
+++ scripts/helena/waterfall_dyneval.R
@@ -14,8 +14,8 @@ state_network<-data_frame(from=names(which(wf$value==min(wf$value))),
 
 ## state_percentages ##
 state_percentages<-matrix(rep(0,length(wf$value)*2),ncol=2)
-state_percentages[,1]<-as.numeric(wf$value)
-state_percentages[,2]<-1-(as.numeric(wf$value))
+state_percentages[,1]<-1-(as.numeric(wf$value))
+state_percentages[,2]<-as.numeric(wf$value)
 colnames(state_percentages)<-c(names(which(wf$value==min(wf$value))),
                                names(which(wf$value==max(wf$value))))
 rownames(state_percentages)<-names(wf$value)
