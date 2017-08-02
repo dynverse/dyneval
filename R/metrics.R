2bcb4b538a3aa2d50778a9cecd990189ac6527a1
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-02 17:02:15 +0200

    fixed bug with phantom edges when they are already connected

--- R/metrics.R
+++ R/metrics.R
@@ -119,6 +119,8 @@ compute_emlike_dist <- function(traj) {
     phantom_edges <-
       expand.grid(from = sn_filt$to, to = sn_filt$to, stringsAsFactors = F) %>%
       filter(from < to) %>%
+      left_join(state_network, by = c("from", "to")) %>%
+      filter(is.na(length)) %>%
       mutate(length = dis_vec[from] + dis_vec[to])
     phantom_edges
   }))
