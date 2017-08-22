98d6f9c65034e70876882ef8115878bb28efd67a
Wouter Saelens <wouter.saelens@gmail.com>
2017-08-22 12:44:56 +0200

    added slingshot

--- scripts/robrecht/one_run.R
+++ scripts/robrecht/one_run.R
@@ -8,9 +8,9 @@ output_root_folder <- "results/output_dyngentest/"
 
 ## load datasets
 .datasets_location = "../dyngen/results/4/" # needs to be defined, to let dyngen know where the datasets are
-# tasks <- load_datasets(8) # this function takes way too long due to the geodesic distances being calculated
+tasks <- load_datasets(8, ndatasets = 10) # this function takes way too long due to the geodesic distances being calculated
 # saveRDS(tasks, paste0(.datasets_location, "tasks.rds"))
-tasks <- readRDS(paste0(.datasets_location, "tasks.rds")) %>% mutate(group = paste0(platform_id, "_", takesetting_type)) %>% group_by(group, ti_type) %>% mutate(subtask_ix = seq_len(n())) %>% ungroup()
+#tasks <- readRDS(paste0(.datasets_location, "tasks.rds")) %>% mutate(group = paste0(platform_id, "_", takesetting_type)) %>% group_by(group, ti_type) %>% mutate(subtask_ix = seq_len(n())) %>% ungroup()
 
 ## take one task
 counts <- tasks$counts[[1]]
@@ -20,6 +20,7 @@ counts <- tasks$counts[[1]]
 # method <- description_waterfall()
 #method <- description_monocle_ddrtree()
 method <- description_scorpius()
+method <- description_slingshot()
 method_fun <- make_obj_fun(method)
 
 ## apply default params
@@ -27,7 +28,7 @@ default_params <- generateDesignOfDefaults(method$par_set)
 params <- generateDesign(n = 20, par.set = method$par_set)
 
 #eval_out <- method_fun(default_params[1,] %>% as.list, tasks = tasks)
-eval_out <- method_fun(list(), tasks = tasks[1:10,])
+eval_out <- method_fun(list(), tasks = tasks[1:min(nrow(tasks), 10),])
 eval_extras <- attr(eval_out,"extras")
 attr(eval_out,"extras") <- NULL
 eval_out
