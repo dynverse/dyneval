890466f5e98faa58ebf6676b3f4bde86b28c1d2e
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-29 15:39:32 +0200

    forgot to attach data io file

--- R/data_io.R
+++ R/data_io.R
@@ -1,3 +1,17 @@
+#' Load dyngen datasets meta information
+#'
+#' \strong{Important!} A variable \code{.datasets_location} needs to be defined in the global environment for this function
+#' to work correctly.
+#'
+#' @return The meta information of the datasets
+#' @export
+#'
+#' @examples
+#' load_datasets_info()
+load_datasets_info <- function() {
+  readRDS(paste0(.datasets_location, "/datasets.rds"))
+}
+
 
 #' Load dyngen datasets
 #'
@@ -5,22 +19,16 @@
 #' to work correctly.
 #'
 #' @param mc_cores The number of cores to use while loading in the datasets (default: 1)
-#' @param num_datasets The maximum number of datasets to load (default: Inf)
+#' @param datasets_info The meta information of the datasets to read
 #'
 #' @return A tibble of datasets
 #' @export
 #'
 #' @examples
-#' datasets <- load_datasets(mc_cores = 1, num_datasets = Inf)
-load_datasets <- function(mc_cores = 1, num_datasets = Inf) {
-  # read datasets meta information
-  datasets_info <- readRDS(paste0(.datasets_location, "/datasets.rds"))
-
-  # determine the number of datasets to read
-  num_datasets <- min(nrow(datasets_info), num_datasets)
-
+#' datasets <- load_datasets(mc_cores = 1, datasets_info = load_datasets_info()))
+load_datasets <- function(mc_cores = 1, datasets_info = load_datasets_info()) {
   # load the datasets one by one
-  task_wrapped <- parallel::mclapply(seq_len(num_datasets), mc.cores = mc_cores, function(dataset_num) {
+  task_wrapped <- parallel::mclapply(seq_len(nrow(datasets_info)), mc.cores = mc_cores, function(dataset_num) {
     dataset_id <- datasets_info$id[[dataset_num]]
     dataset <- dyngen::load_dataset(dataset_id)
 
