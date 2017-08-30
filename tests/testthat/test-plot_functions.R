6cc655b954b9cdaae4ac8e28d2d916599001e77b
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-30 13:35:15 +0200

    rework dimred plotting function

--- tests/testthat/test-plot_functions.R
+++ tests/testthat/test-plot_functions.R
@@ -1,24 +1,24 @@
 context("Plot functions")
 
-# datasets <- source_file("helper-datasets-load.R", chdir = F)
-datasets <- readRDS(paste0(tempdir(), "/dyneval_test_datasets.rds"))
+tasks <- readRDS(paste0(tempdir(), "/dyneval_test_datasets.rds"))
 
-test_that("perform dimred on trajectory", {
-  for (dataseti in seq_len(nrow(datasets))) {
-    dataset <- extract_row_to_list(datasets, dataseti)
-    dimred <- plotdata_default(dataset, insert_phantom_edges = T)
+for (taski in seq_len(nrow(tasks))) {
+  task <- extract_row_to_list(tasks, taski)
+
+  test_that(paste0("Perform dimred on trajectory with task ", task$id), {
+    dimred <- dimred_trajectory(task, insert_phantom_edges = T)
 
     # check dimred$space_milestones
-    milestone_ids <- dataset$milestone_ids
+    milestone_ids <- task$milestone_ids
     space_milestones <- dimred$space_milestones
     expect_equal( nrow(space_milestones), length(milestone_ids) )
-    expect_equal( space_milestones$id, milestone_ids )
-    expect_true( all(c("id", "Comp1", "Comp2", "colour") %in% colnames(space_milestones)) )
+    expect_equal( space_milestones$milestone_id, milestone_ids )
+    expect_true( all(c("milestone_id", "Comp1", "Comp2", "colour") %in% colnames(space_milestones)) )
     expect_true( all(is.finite(space_milestones$Comp1)) )
     expect_true( all(is.finite(space_milestones$Comp2)) )
 
     # check dimred$milestone_network
-    milestone_network <- dataset$milestone_network
+    milestone_network <- task$milestone_network
     space_lines <- dimred$space_lines
     expect_equal( nrow(space_lines), nrow(milestone_network) )
     expect_true( all(is.finite(space_lines$from.Comp1)) )
@@ -28,12 +28,13 @@ test_that("perform dimred on trajectory", {
     expect_equal( sort(unique(c(space_lines$from, space_lines$to))), sort(milestone_ids) )
 
     # check dimred$space_samples
+    cell_ids <- task$cell_ids
     space_samples <- dimred$space_samples
-    cell_ids <- dataset$cell_ids
     expect_equal( nrow(space_samples), length(cell_ids) )
+    expect_equal( space_samples$cell_id, cell_ids )
 
     # try different param
-    dimred2 <- plotdata_default(dataset, insert_phantom_edges = F)
+    dimred2 <- dimred_trajectory(task, insert_phantom_edges = F)
     # dimred2
-  }
-})
+  })
+}
