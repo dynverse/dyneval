6e72b1898197b857669820b0f77d87f6dfa36828
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-30 09:39:56 +0200

    clean up load datasets functions

--- tests/testthat/helper-executefirst-loaddatasets.R
+++ tests/testthat/helper-executefirst-loaddatasets.R
@@ -1,7 +1,16 @@
-filename <- paste0(tempdir(), "/dyneval_test_datasets.rds")
+# where are the dyngen datasets stored?
 .datasets_location = "../../../dyngen/results/4/"
+# .datasets_location = "../dyngen/results/4/"
+
+# set a seed for replication reasons
 set.seed(1)
-datasets_sel <- dplyr::sample_n(load_datasets_info(), 16)
-cat("Loading ", nrow(datasets_sel), " datasets\n", sep="")
-datasets <- load_datasets(mc_cores = 8, datasets_sel)
-saveRDS(datasets, filename)
+
+# sample some datasets
+task_sel <- dplyr::sample_n(load_datasets_info(), 16)
+
+# load them
+cat("Loading ", nrow(task_sel), " datasets\n", sep="")
+tasks <- load_datasets(mc_cores = 8, task_sel)
+
+# save file
+saveRDS(tasks, paste0(tempdir(), "/dyneval_test_datasets.rds"))
