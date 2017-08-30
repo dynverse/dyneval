38e4affdbfaf2cc9cf3c8e0b7284c3e2e78f508e
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-30 16:57:09 +0200

    * retain classes after tibble wrapping * remove duplicated code from plot functions

--- tests/testthat/test-plot_functions.R
+++ tests/testthat/test-plot_functions.R
@@ -6,6 +6,7 @@ for (taski in seq_len(nrow(tasks))) {
   task <- extract_row_to_list(tasks, taski)
 
   test_that(paste0("Perform dimred on trajectory with task ", task$id), {
-
+    g <- plot_default(task)
+    expect_is(g$layers[[1]], "ggproto")
   })
 }
