1be4323142a1adcc83f44685e4f20fd051727519
Robrecht Cannoodt <brechtus@gmail.com>
2017-08-31 10:05:11 +0200

    fixing get_descriptions, adding bioconductor remotes

---
+++ tests/testthat/test-wrap_ti_method.R
@@ -0,0 +1,9 @@
+context("Getting all descriptions")
+
+tib <- get_descriptions()
+expect_that(tib, is_a("tbl"))
+
+lis <- get_descriptions(as_tibble = F)
+expect_that(lis, is_a("list"))
+
+check_dependencies()
