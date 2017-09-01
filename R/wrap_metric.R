3ccd7a820b333a9ba0ce9e5f14df9cd4c68f8b1b
Robrecht Cannoodt <brechtus@gmail.com>
2017-09-01 15:34:41 +0200

    don't need to split anymore

--- R/wrap_metric.R
+++ R/wrap_metric.R
@@ -365,8 +365,6 @@ compute_coranking <- function(gold_dist, pred_dist) {
 #' @importFrom GEDEVO poor_mans_wrap
 #' @export
 calculate_ged <- function(net1, net2) {
-  gedevo_path <- paste0(find.package("dyneval"), "/extra_code/GEDEVO/linux-x64/gedevo")
-
   net1 <- net1 %>% mutate(dir="u") %>% select(from, dir, to)
   net2 <- net2 %>% mutate(dir="u") %>% select(from, dir, to)
 
@@ -376,8 +374,7 @@ calculate_ged <- function(net1, net2) {
   write.table(net2, file.path(tempfolder, "net2.sif"), row.names = FALSE, col.names = FALSE)
 
   cmd <- glue::glue("gedevo --groups a b --sif {tempfolder}/net1.sif a --sif {tempfolder}/net2.sif b --no-prematch --no-workfiles --save {tempfolder}/out --maxiter 100 --maxsecs 1 --maxsame 100")
-  args <- strsplit(cmd, " ")[[1]]
-  GEDEVO::poor_mans_wrap(args)
+  GEDEVO::run_GEDEVO(cmd)
 
   score <- read_file(paste0(tempfolder, "/out.matching")) %>% gsub("^.*GED score:[ ]*([0-9\\.]*).*", "\\1", .) %>% as.numeric()
 
