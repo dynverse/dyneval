103420b12026f8b5ec6c88d29e06b27217e0d235
Wouter Saelens <wouter.saelens@gmail.com>
2017-09-11 14:01:15 +0200

    added phenopath

---
+++ R/ti_phenopath.R
@@ -0,0 +1,52 @@
+#' Description for phenopath
+#' @export
+description_phenopath <- function() create_description(
+  name = "phenopath",
+  short_name = "phenopath",
+  package_required = c("phenopath"),
+  package_loaded = c(),
+  par_set = makeParamSet(
+    makeIntegerParam(id = "thin", lower = 2, upper = 500, default = 40),
+    makeDiscreteParam(id = "z_init", default = 1, values = c(1, 2, 3, 4, 5, "random")),
+    makeLogicalParam(id="model_mu", default=FALSE),
+    makeLogicalParam(id="scale_y", default=TRUE)
+  ),
+  properties = c(),
+  run_fun = run_phenopath,
+  plot_fun = plot_phenopath
+)
+
+run_phenopath <- function(
+    counts,
+    thin = 40,
+    z_init = 1,
+    model_mu = FALSE,
+    scale_y = TRUE
+  ) {
+  requireNamespace("phenopath")
+
+  fit <- phenopath::phenopath(counts, rep(1, nrow(counts)), elbo_tol = 1e-6, thin = , z_init=z_init)
+  time <- phenopath::trajectory(fit)
+
+  milestone_ids <- c("milestone_A", "milestone_B")
+  milestone_network <- tibble::data_frame(from = milestone_ids[[1]], to = milestone_ids[[2]], length = 1)
+  progressions <- tibble(
+    cell_id = rownames(counts),
+    from = milestone_ids[[1]],
+    to = milestone_ids[[2]],
+    percentage=pseudotimes
+  )
+
+  wrap_ti_prediction(
+    ti_type = "linear",
+    id = "phenopath",
+    cell_ids = rownames(counts),
+    milestone_ids = milestone_ids,
+    milestone_network = milestone_network,
+    progressions = progressions,
+    oui = oui
+  )
+}
+
+#' @import ggplot2
+plot_phenopath <- plot_default
