53261741690fb88353fa10e1c31c2b1901707921
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-09-14 11:29:27 +0200

    fix script tibble

--- scripts/robrecht/training_parameters_cv_cluster.R
+++ scripts/robrecht/training_parameters_cv_cluster.R
@@ -12,7 +12,7 @@ dir.create(out_dir, recursive = T)
 select_tasks <- generate_toy_datasets()
 task_group <- rep("group", nrow(select_tasks))
 task_fold <- gsub(".*_", "", select_tasks$id) %>% as.integer()
-methods <- get_descriptions(as_tibble = F)
+methods <- get_descriptions(as_tibble = T)
 benchmark_suite_submit(select_tasks, task_group, task_fold, methods = methods, out_dir = out_dir)
 
 
