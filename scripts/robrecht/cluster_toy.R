7e5f136975253d228b88076b551925b43434f1a1
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-09-15 14:44:49 +0200

    add cluster_toy script

---
+++ scripts/robrecht/cluster_toy.R
@@ -0,0 +1,26 @@
+library(dyneval)
+library(tidyverse)
+
+out_dir <- "~/Workspace/dynverse/dynresults/cluster_toy/"
+dir.create(out_dir, recursive = T)
+
+# easy test
+select_tasks <- generate_toy_datasets(num_replicates = 2)
+task_group <- rep("group", nrow(select_tasks))
+task_fold <- gsub(".*_", "", select_tasks$id) %>% as.integer()
+methods <- get_descriptions(as_tibble = T)
+benchmark_suite_submit(
+  select_tasks,
+  task_group,
+  task_fold,
+  out_dir = out_dir,
+  methods = methods,
+  metrics = c("auc_R_nx", "robbie_network_score"),
+  timeout = 600,
+  memory = "16G",
+  num_cores = 2,
+  num_iterations = 5,
+  num_init_params = 16
+)
+
+benchmark_suite_retrieve(out_dir)
