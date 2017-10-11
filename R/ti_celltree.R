9d83853990fd02ade5d8e2cc204b9bb2999571c3
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-11 13:31:35 +0200

    add abstract description function for celltree as well

--- R/ti_celltree.R
+++ R/ti_celltree.R
@@ -1,70 +1,67 @@
 #' Description for celltree maptpx
 #' @export
-description_celltree_maptpx <- function() create_description(
-  name = "cellTree with maptpx",
-  short_name = "CTmaptpx",
-  package_loaded = c("cellTree"),
-  package_required = c(),
-  par_set = makeParamSet(
-    makeDiscreteParam(id = "method", values = "maptpx", default = "maptpx"),
-    makeIntegerParam(id = "num_topics_lower", lower = 2L, upper = 15L, default = 2L),
-    makeIntegerParam(id = "num_topics_upper", lower = 2L, upper = 15L, default = 15L),
-    makeNumericParam(id = "sd_filter", lower = log(.01), upper = log(5.0), default = log(.5), special.vals = list(FALSE), trafo = exp),
-    makeNumericParam(id = "tot_iter", lower = log(10^4), upper = log(10^7), default = log(10^6), trafo = function(x) as.integer(round(exp(x)))),
-    makeNumericParam(id = "tolerance", lower = log(.001), upper = log(.5), default = log(.05), trafo = exp),
-    makeNumericParam(id = "width_scale_factor", lower = 1.01, default = 1.2, upper = 2),
-    forbidden = quote(num_topics_lower > num_topics_upper)
-  ),
-  properties = c(),
-  run_fun = run_celltree,
-  plot_fun = plot_celltree
-)
+description_celltree_maptpx <- function() abstract_celltree_description("maptpx")
 
 #' Description for celltree gibbs
 #' @export
-description_celltree_gibbs <- function() create_description(
-  name = "cellTree with Gibbs",
-  short_name = "CTGibbs",
-  package_loaded = c("cellTree"),
-  package_required = c(),
-  par_set = makeParamSet(
-    makeDiscreteParam(id = "method", values = "Gibbs", default = "Gibbs"),
-    makeIntegerParam(id = "num_topics", lower = 2L, default = 4L, upper = 15L),
-    makeNumericParam(id = "sd_filter", lower = log(.01), upper = log(5.0), default = log(.5), special.vals = list(FALSE), trafo = exp),
-    makeNumericParam(id = "tot_iter", lower = log(50), upper = log(500), default = log(200), trafo = function(x) round(exp(x))),
-    makeNumericParam(id = "tolerance", lower = log(10^-7), upper = log(10^-3), default = log(10^-5), trafo = exp),
-    makeNumericParam(id = "width_scale_factor", lower = log(.1), default = log(1.2), upper = log(100), trafo = exp)
-  ),
-  properties = c(),
-  run_fun = run_celltree,
-  plot_fun = plot_celltree
-)
+description_celltree_gibbs <- function() abstract_celltree_description("Gibbs")
 
-#' Description for celltree VEM
+#' Description for celltree vem
 #' @export
-description_celltree_vem <- function() create_description(
-  name = "cellTree with VEM",
-  short_name = "CTVEM",
-  package_loaded = c("cellTree"),
-  package_required = c(),
-  par_set = makeParamSet(
-    makeDiscreteParam(id = "method", values = "VEM", default = "VEM"),
-    makeIntegerParam(id = "num_topics", lower = 2L, default = 4L, upper = 15L),
-    makeNumericParam(id = "sd_filter", lower = log(.01), upper = log(5.0), default = log(.5), special.vals = list(FALSE), trafo = exp),
-    makeNumericParam(id = "tot_iter", lower = log(10^4), upper = log(10^7), default = log(10^6), trafo = function(x) round(exp(x))),
-    makeNumericParam(id = "tolerance", lower = log(10^-7), upper = log(10^-3), default = log(10^-5), trafo = exp),
-    makeNumericParam(id = "width_scale_factor", lower = log(.1), default = log(1.5), upper = log(100), trafo = exp)
-  ),
-  properties = c(),
-  run_fun = run_celltree,
-  plot_fun = plot_celltree
-)
+description_celltree_vem <- function() abstract_celltree_description("VEM")
+
+abstract_celltree_description <- function(method) {
+  if (method == "maptpx") {
+    par_set <- makeParamSet(
+      makeDiscreteParam(id = "method", values = "maptpx", default = "maptpx"),
+      makeIntegerParam(id = "num_topics_lower", lower = 2L, upper = 15L, default = 2L),
+      makeIntegerParam(id = "num_topics_upper", lower = 2L, upper = 15L, default = 15L),
+      makeNumericParam(id = "sd_filter", lower = log(.01), upper = log(5.0), default = log(.5), special.vals = list(FALSE), trafo = exp),
+      makeNumericParam(id = "tot_iter", lower = log(10^4), upper = log(10^7), default = log(10^6), trafo = function(x) as.integer(round(exp(x)))),
+      makeNumericParam(id = "tolerance", lower = log(.001), upper = log(.5), default = log(.05), trafo = exp),
+      makeNumericParam(id = "width_scale_factor", lower = 1.01, default = 1.2, upper = 2),
+      forbidden = quote(num_topics_lower > num_topics_upper)
+    )
+  } else if (method == "Gibbs") {
+    par_set <- makeParamSet(
+      makeDiscreteParam(id = "method", values = "Gibbs", default = "Gibbs"),
+      makeIntegerParam(id = "num_topics", lower = 2L, default = 4L, upper = 15L),
+      makeNumericParam(id = "sd_filter", lower = log(.01), upper = log(5.0), default = log(.5), special.vals = list(FALSE), trafo = exp),
+      makeNumericParam(id = "tot_iter", lower = log(50), upper = log(500), default = log(200), trafo = function(x) round(exp(x))),
+      makeNumericParam(id = "tolerance", lower = log(10^-7), upper = log(10^-3), default = log(10^-5), trafo = exp),
+      makeNumericParam(id = "width_scale_factor", lower = log(.1), default = log(1.2), upper = log(100), trafo = exp)
+    )
+  } else if (method == "VEM") {
+    par_set <- makeParamSet(
+      makeDiscreteParam(id = "method", values = "VEM", default = "VEM"),
+      makeIntegerParam(id = "num_topics", lower = 2L, default = 4L, upper = 15L),
+      makeNumericParam(id = "sd_filter", lower = log(.01), upper = log(5.0), default = log(.5), special.vals = list(FALSE), trafo = exp),
+      makeNumericParam(id = "tot_iter", lower = log(10^4), upper = log(10^7), default = log(10^6), trafo = function(x) round(exp(x))),
+      makeNumericParam(id = "tolerance", lower = log(10^-7), upper = log(10^-3), default = log(10^-5), trafo = exp),
+      makeNumericParam(id = "width_scale_factor", lower = log(.1), default = log(1.5), upper = log(100), trafo = exp)
+    )
+  }
+
+  run_fun <- run_celltree
+  formals(run_fun)$method <- method
+
+  create_description(
+    name = glue::glue("cellTree with {method}"),
+    short_name = glue::glue("CT{method}"),
+    package_loaded = c("cellTree"),
+    package_required = c(),
+    par_set = par_set,
+    properties = c(),
+    run_fun = run_fun,
+    plot_fun = plot_celltree
+  )
+}
 
 #' @importFrom igraph degree distances get.vertex.attribute induced_subgraph
 #' @importFrom reshape2 melt
 run_celltree <- function(counts,
-                         start_cell_id=NULL,
-                         cell_grouping=NULL,
+                         start_cell_id = NULL,
+                         cell_grouping = NULL,
                          method = "maptpx",
                          num_topics_lower = 2,
                          num_topics_upper = 15,
@@ -73,7 +70,7 @@ run_celltree <- function(counts,
                          tot_iter = 1e6,
                          tolerance = .05,
                          width_scale_factor = 1.5
-  ) {
+) {
   requireNamespace("cellTree")
 
   expression <- log2(counts+1)
