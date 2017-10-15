f49415a110839d7066916811c76ca2d80f0d3ca1
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-15 11:38:55 +0200

    add discard to purrr imports

--- R/package.R
+++ R/package.R
@@ -9,7 +9,7 @@
 #' @import dynutils
 #' @importFrom stats cor dist kmeans median prcomp quantile runif setNames step time
 #' @importFrom utils installed.packages head tail
-#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep set_names
+#' @importFrom purrr %>% map map_df map_chr map_lgl map_int map_dbl keep discard set_names
 #' @importFrom magrittr %<>% %$%
 #' @importFrom cowplot theme_cowplot
 #'
