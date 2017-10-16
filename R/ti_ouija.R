8b0710756bd3de6a32c9644cf63812b8c2b59550
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-16 13:22:54 +0200

    added pca visualisation to ouija

--- R/ti_ouija.R
+++ R/ti_ouija.R
@@ -44,8 +44,14 @@ run_ouija <- function(
   )
 
   # obtain the pseudotimes
-  pseudotimes <- ouija::map_pseudotime(oui)
-  pseudotimes <- (pseudotimes - min(pseudotimes)) / (max(pseudotimes) - min(pseudotimes))
+  pseudotimes <- ouija::map_pseudotime(oui) %>% dynutils::scale_minmax()
+
+  # run pca for visualisation purposes
+  space <- stats::prcomp(expr)$x[,1:2] %>%
+    as.data.frame() %>%
+    rownames_to_column() %>%
+    as_data_frame() %>%
+    mutate(pseudotime = pseudotimes)
 
   # produce output objects
   milestone_ids <- c("milestone_A", "milestone_B")
@@ -53,7 +59,7 @@ run_ouija <- function(
     from = milestone_ids[[1]],
     to = milestone_ids[[2]],
     length = 1,
-    directed = TRUE
+    directed = FALSE
   )
   progressions <- data_frame(
     cell_id = rownames(counts),
@@ -82,28 +88,42 @@ run_ouija <- function(
     milestone_ids = milestone_ids,
     milestone_network = milestone_network,
     progressions = progressions,
-    t0_df = t0_df
+    t0_df = t0_df,
+    space = space
   )
 }
 
 #' @importFrom cowplot theme_cowplot
-#' @importFrom viridis viridis_pal
-plot_ouija <- function(prediction) {
-  requireNamespace("ouija")
+#' @importFrom viridis viridis_pal scale_colour_viridis
+plot_ouija <- function(prediction, type = c("dimred", "switch_times")) {
+  type <- match.arg(type)
 
-  t0_df <- prediction$t0_df
+  switch(
+    type,
+    dimred = {
+      ggplot(prediction$space) +
+        geom_point(aes(PC1, PC2, colour = pseudotime)) +
+        viridis::scale_colour_viridis() +
+        cowplot::theme_cowplot()
+    },
+    switch_times = {
+      requireNamespace("ouija")
 
-  # adapted from ouija::plot_switch_times(prediction$oui)
-  # to avoid saving the whole oui file
-  vpal <- viridis::viridis_pal()(8)
+      t0_df <- prediction$t0_df
 
-  ggplot(t0_df, aes(x = Gene, y = t0_mean, fill = kmean)) +
-    geom_errorbar(aes(ymin = lower, ymax = upper), color = "grey60", width = 0.5, alpha = 0.5) +
-    coord_flip() +
-    geom_point(color = "grey50", shape = 21, size = 3) +
-    ylab("Switch point") +
-    scale_fill_gradient2(name = "Regulation", low = vpal[1], high = vpal[5]) +
-    scale_color_gradient2(name = "Regulation", low = vpal[1], high = vpal[5]) +
-    theme(legend.position = "top") +
-    cowplot::theme_cowplot()
+      # adapted from ouija::plot_switch_times(prediction$oui)
+      # to avoid saving the whole oui file
+      vpal <- viridis::viridis_pal()(8)
+
+      ggplot(t0_df, aes(x = Gene, y = t0_mean, fill = kmean)) +
+        geom_errorbar(aes(ymin = lower, ymax = upper), color = "grey60", width = 0.5, alpha = 0.5) +
+        coord_flip() +
+        geom_point(color = "grey50", shape = 21, size = 3) +
+        ylab("Switch point") +
+        scale_fill_gradient2(name = "Regulation", low = vpal[1], high = vpal[5]) +
+        scale_color_gradient2(name = "Regulation", low = vpal[1], high = vpal[5]) +
+        theme(legend.position = "top") +
+        cowplot::theme_cowplot()
+    }
+  )
 }
