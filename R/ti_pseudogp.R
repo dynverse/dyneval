133ba92555abd52dce020128fe19bbbe454a6f78
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-17 22:26:38 +0200

    fix default param in pseudogp

--- R/ti_pseudogp.R
+++ R/ti_pseudogp.R
@@ -10,8 +10,8 @@ description_pseudogp <- function() create_description(
     makeNumericParam(id = "smoothing_beta", lower = 1, upper = 20, default = 3),
     makeNumericParam(id = "pseudotime_mean", lower = 0, upper = 1, default = 0.5),
     makeNumericParam(id = "pseudotime_var", lower = 0.01, upper = 1, default = 1),
-    makeIntegerParam(id = "chains", lower = 1L, upper = 20L, default = 1L),
-    makeNumericParam(id = "iter", lower = log(5), upper = log(100), default = log(1000), trafo = function(x) round(exp(x))), # default is 1000
+    makeIntegerParam(id = "chains", lower = 1L, default = 1L, upper = 20L),
+    makeNumericParam(id = "iter", lower = log(5), default = log(100), upper = log(1000), trafo = function(x) round(exp(x))), # default is 1000
     makeLogicalVectorParam(id = "dimreds", len = length(list_dimred_methods()), default = names(list_dimred_methods()) == "pca"),
     makeDiscreteParam(id = "initialise_from", values=c("random", "principal_curve", "pca"), default="random")
   ),
