ed0581e0d25770afc9918bbbb38fb34cbeba0a20
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-17 23:08:40 +0200

    specify certain paramhelpers commit to get rid of discreteNameToValue issue

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,9 @@
-.onLoad <- function(libname, pkgname){
-  packageStartupMessage("Loading discreteNameToValue in your global environment -- this is a dirty fix.")
-  assign(
-    "discreteNameToValue",
-    ParamHelpers::discreteNameToValue,
-    envir = .GlobalEnv
-  )
-}
+# # This can be removed by adding ParamHelpers to DESCRIPTION
+# .onLoad <- function(libname, pkgname){
+#   packageStartupMessage("Loading discreteNameToValue in your global environment -- this is a dirty fix.")
+#   assign(
+#     "discreteNameToValue",
+#     ParamHelpers::discreteNameToValue,
+#     envir = .GlobalEnv
+#   )
+# }
