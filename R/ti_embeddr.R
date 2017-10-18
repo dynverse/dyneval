836f8cb03c6ae81a42458a792b6dd8e61d51270c
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-18 12:43:42 +0200

    implement a dyneval theme

--- R/ti_embeddr.R
+++ R/ti_embeddr.R
@@ -89,9 +89,13 @@ run_embeddr <- function(counts,
   )
 }
 
-plot_embeddr <- function(predictions) {
+plot_embeddr <- function(prediction) {
+  sample_df <- prediction$dimred_samples %>% mutate(time = prediction$pseudotimes)
+  traj_df <- prediction$dimred_traj
   ggplot() +
-    geom_point(aes(component_1, component_2), predictions$dimred_samples, alpha = .65, size = 3.5) +
-    geom_path(aes(trajectory_1, trajectory_2), predictions$dimred_traj, size = 1.5, alpha = 0.8, linetype = 2) +
-    cowplot::theme_cowplot()
+    geom_point(aes(component_1, component_2, fill = time), sample_df, pch = 21, alpha = .65, size = 3.5) +
+    geom_path(aes(trajectory_1, trajectory_2), traj_df, size = 1.5, alpha = 0.8, linetype = 2) +
+    scale_fill_distiller(palette = "YlOrRd") +
+    scale_colour_distiller(palette = "YlOrRd") +
+    theme_dyneval()
 }
