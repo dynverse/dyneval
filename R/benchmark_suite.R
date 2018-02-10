580d56ef833ae86e3fddef2adfeb32f14b631810
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-10 09:28:35 +0100

    splitting the retrieve function into two separate sections, will split it up into two separate functions in the future

--- R/benchmark_suite.R
+++ R/benchmark_suite.R
@@ -370,98 +370,100 @@ benchmark_suite_retrieve <- function(out_dir, return_outputs = TRUE) {
   map_df(method_names, function(method_name) {
     method_folder <- paste0(out_dir, method_name)
     output_file <- paste0(method_folder, "/output.rds")
-    qsubhandle_file <- paste0(method_folder, "/qsubhandle.rds")
+    qsubhandle_file <- paste0
 
-    if (file.exists(output_file)) {
-      cat(method_name, ": Reading previous output\n", sep = "")
-      if (return_outputs) {
+    if (return_outputs) {
+      if (file.exists(output_file)) {
+        cat(method_name, ": Reading previous output\n", sep = "")
         readr::read_rds(output_file)
       } else {
+        cat(method_name, ": Output not found, skipping\n", sep = "")
         NULL
       }
-    } else if (!file.exists(output_file) && file.exists(qsubhandle_file)) {
-      cat(method_name, ": Attempting to retrieve output from cluster: ", sep = "")
-      data <- readr::read_rds(qsubhandle_file)
-      grid <- data$grid
-      qsub_handle <- data$qsub_handle
-      num_tasks <- qsub_handle$num_tasks
+    } else
+    {
+      if (!file.exists(output_file) && file.exists(qsubhandle_file)) {
+        cat(method_name, ": Attempting to retrieve output from cluster: ", sep = "")
+        data <- readr::read_rds(qsubhandle_file)
+        grid <- data$grid
+        qsub_handle <- data$qsub_handle
+        num_tasks <- qsub_handle$num_tasks
 
-      output <- PRISM::qsub_retrieve(
-        qsub_handle,
-        wait = FALSE
-      )
+        output <- PRISM::qsub_retrieve(
+          qsub_handle,
+          wait = FALSE
+        )
 
-      # suppressing inevitable warnings:
-      # when the job is still partly running, calling qacct will generate a warning
-      # when the job is finished, calling qstat will generate a warning
-      suppressWarnings({
-        qacct_out <- PRISM::qacct(qsub_handle)
-        qstat_out <- PRISM::qstat_j(qsub_handle)
-      })
+        # suppressing inevitable warnings:
+        # when the job is still partly running, calling qacct will generate a warning
+        # when the job is finished, calling qstat will generate a warning
+        suppressWarnings({
+          qacct_out <- PRISM::qacct(qsub_handle)
+          qstat_out <- PRISM::qstat_j(qsub_handle)
+        })
 
-      if (!is.null(output)) {
-        cat("Output found! Saving output.\n", sep = "")
+        if (!is.null(output)) {
+          cat("Output found! Saving output.\n", sep = "")
 
-        output_succeeded <- TRUE
-        outputs <- map_df(seq_along(output), function(grid_i) {
-          out <- output[[grid_i]]
-          if(length(out) != 1 || !is.na(out)) {
-            out
-          } else {
-            qsub_error <- attr(out, "qsub_error")
+          output_succeeded <- TRUE
+          outputs <- map_df(seq_along(output), function(grid_i) {
+            out <- output[[grid_i]]
+            if(length(out) != 1 || !is.na(out)) {
+              out
+            } else {
+              qsub_error <- attr(out, "qsub_error")
 
-            qsub_memory <- qsub_handle$memory %>% str_replace("G$", "") %>% as.numeric
-            qacct_memory <- qacct_out$maxvmem %>% str_replace("GB$", "") %>% as.numeric
+              qsub_memory <- qsub_handle$memory %>% str_replace("G$", "") %>% as.numeric
+              qacct_memory <- qacct_out$maxvmem %>% str_replace("GB$", "") %>% as.numeric
 
-            if (!is.na(qacct_memory) && length(qacct_memory) > 0 && qacct_memory > qsub_memory) {
-              qsub_error <- "Memory limit exceeded"
+              if (!is.na(qacct_memory) && length(qacct_memory) > 0 && qacct_memory > qsub_memory) {
+                qsub_error <- "Memory limit exceeded"
+              }
+
+              # mimic eval_ind format
+              data_frame(
+                method_name = data$method$name,
+                method_short_name = data$method$short_name,
+                error_message = qsub_error,
+                repeat_i = grid$repeat_i[[grid_i]],
+                fold_i = grid$fold_i[[grid_i]],
+                group_sel = grid$group_sel[[grid_i]],
+                grid_i
+              )
+            }
+          })
+        } else {
+          error_message <-
+            if (is.null(qstat_out) || nrow(qstat_out) > 0) {
+              "job is still running"
+            } else {
+              "qsub_retrieve of results failed -- no output was produced, but job is not running any more"
             }
 
-            # mimic eval_ind format
-            data_frame(
-              method_name = data$method$name,
-              method_short_name = data$method$short_name,
-              error_message = qsub_error,
-              repeat_i = grid$repeat_i[[grid_i]],
-              fold_i = grid$fold_i[[grid_i]],
-              group_sel = grid$group_sel[[grid_i]],
-              grid_i
-            )
-          }
-        })
+          cat("Output not found. ", error_message, ".\n", sep = "")
+          output_succeeded <- FALSE
+
+          NULL
+        }
+
+        if (output_succeeded) {
+          readr::write_rds(outputs, output_file)
+        }
+
+        gc()
+
+        NULL
+
       } else {
-        error_message <-
-          if (is.null(qstat_out) || nrow(qstat_out) > 0) {
-            "job is still running"
-          } else {
-            "qsub_retrieve of results failed -- no output was produced, but job is not running any more"
-          }
+        if (file.exists(output_file)) {
+          cat(method_name, ": Output already present", sep = "")
+        } else {
+          cat(method_name, ": No qsub file was found", sep = "")
+        }
 
-        cat("Output not found. ", error_message, ".\n", sep = "")
-        output_succeeded <- FALSE
-
-        # mimic eval_ind format
-        outputs <- grid %>% mutate(
-          grid_i = seq_len(n()),
-          method_name = data$method$name,
-          method_short_name = data$method$short_name,
-          error_message = error_message
-        )
+        NULL
       }
 
-      if (output_succeeded) {
-        readr::write_rds(outputs, output_file)
-      }
-
-      if (!return_outputs) {
-        outputs <- NULL
-      }
-
-      gc()
-
-      outputs
-    } else {
-      stop("Could not find an output.rds or qsubhandle.rds file in out_dir = ", sQuote(out_dir))
     }
   })
 }
