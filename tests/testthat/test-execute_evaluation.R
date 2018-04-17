75a5c9ca6bd585ec485fe6672ec8e52622902ec5
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-17 17:11:06 +0200

    extend test with random, error and identity

--- tests/testthat/test-execute_evaluation.R
+++ tests/testthat/test-execute_evaluation.R
@@ -1,6 +1,6 @@
 context("Testing execute_evaluation")
 
-test_that(paste0("Testing execute_evaluation"), {
+test_that(paste0("Testing execute_evaluation with random"), {
   out <- execute_evaluation(
     tasks = dyntoy::toy_tasks[5,],
     method = dynmethods::description_random(),
@@ -13,6 +13,67 @@ test_that(paste0("Testing execute_evaluation"), {
   )
 
   expect_is(out, "numeric")
+
+  summary <- attr(out, "extras")$.summary
+  models <- attr(out, "extras")$.models
+
+  expect_null(summary$error[[1]])
+
+  expect_is(summary$correlation, "numeric")
+
+  expect_is(summary$edge_flip, "numeric")
+
+  expect_is(summary$rf_mse, "numeric")
+
+  expect_true(dynwrap::is_wrapper_with_trajectory(models[[1]]))
+})
+
+
+
+test_that(paste0("Testing execute_evaluation with error"), {
+  out <- execute_evaluation(
+    tasks = dyntoy::toy_tasks[5,],
+    method = dynmethods::description_error(),
+    parameters = list(),
+    metrics = c("correlation", "edge_flip", "rf_mse"),
+    output_model = TRUE,
+    extra_metrics = NULL,
+    mc_cores = 2,
+    verbose = TRUE
+  )
+
+  expect_is(out, "numeric")
+  expect_true(all(out == c(0, 0, 1)))
+
+  summary <- attr(out, "extras")$.summary
+  models <- attr(out, "extras")$.models
+
+  expect_true(!is.null(summary$error[[1]]))
+
+  expect_is(summary$correlation, "numeric")
+
+  expect_is(summary$edge_flip, "numeric")
+
+  expect_is(summary$rf_mse, "numeric")
+
+  expect_true(is.null(models[[1]]))
+})
+
+
+test_that(paste0("Testing execute_evaluation with identity"), {
+  out <- execute_evaluation(
+    tasks = dyntoy::toy_tasks[5,],
+    method = dynmethods::description_identity(),
+    parameters = list(),
+    metrics = c("correlation", "edge_flip", "rf_mse"),
+    output_model = TRUE,
+    extra_metrics = NULL,
+    mc_cores = 2,
+    verbose = TRUE
+  )
+
+  expect_is(out, "numeric")
+  expect_true(all(out - c(1, 1, 0) < .001))
 
   summary <- attr(out, "extras")$.summary
   models <- attr(out, "extras")$.models
