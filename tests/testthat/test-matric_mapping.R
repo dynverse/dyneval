b7cc9bd9046fdb838b44a95028a07af20b100537
zouter <wouter.saelens@gmail.com>
2018-08-10 12:19:16 +0200

    rename the tests

--- tests/testthat/test-matric_mapping.R
+++ tests/testthat/test-matric_mapping.R
@@ -1,13 +1,19 @@
-context("Branch overlap")
+context("Mapping scores")
 
 # generate a simple dataset for comparison
 dataset <- dyntoy::generate_dataset(model = "multifurcating", num_features = 2, allow_tented_progressions = FALSE, normalise = FALSE)
 
-test_that(paste0("Branch overlap returns relevant results"), {
+test_that(paste0("Mapping scores return relevant results"), {
   # when exact gold standard -> all scores = 1
   prediction <- dataset
 
-  results <- calculate_branch_overlap(dataset, prediction)
+  results <- calculate_mapping_branches(dataset, prediction)
+  testthat::expect_true(all(
+    results$recovery == 1,
+    results$relevance == 1,
+    results$F1 == 1
+  ))
+  results <- calculate_mapping_milestones(dataset, prediction)
   testthat::expect_true(all(
     results$recovery == 1,
     results$relevance == 1,
@@ -17,19 +23,33 @@ test_that(paste0("Branch overlap returns relevant results"), {
   # when only one edge remains in the prediction -> relevance still 1
   prediction <- dataset
   prediction$progressions <- prediction$progressions %>% filter(from == first(from) & to == first(to))
+  prediction$milestone_percentages <- prediction$milestone_percentages %>% filter(cell_id %in% prediction$progressions$cell_id)
 
-  results <- calculate_branch_overlap(dataset, prediction)
+  results <- calculate_mapping_branches(dataset, prediction)
   testthat::expect_true(all(
     results$recovery < 1,
     results$relevance == 1,
+    results$F1 < 1
+  ))
+  results <- calculate_mapping_milestones(dataset, prediction)
+  testthat::expect_true(all(
+    results$recovery < 1,
+    results$relevance < 1,
     results$F1 < 1
   ))
 
   # when all cells are removed -> all scores = 0
   prediction <- dataset
   prediction$progressions <- prediction$progressions %>% filter(FALSE)
+  prediction$milestone_percentages <- prediction$milestone_percentages %>% filter(FALSE)
 
-  results <- calculate_branch_overlap(dataset, prediction)
+  results <- calculate_mapping_branches(dataset, prediction)
+  testthat::expect_true(all(
+    results$recovery == 0,
+    results$relevance == 0,
+    results$F1 == 0
+  ))
+  results <- calculate_mapping_milestones(dataset, prediction)
   testthat::expect_true(all(
     results$recovery == 0,
     results$relevance == 0,
@@ -40,7 +60,8 @@ test_that(paste0("Branch overlap returns relevant results"), {
   prediction <- dataset
   prediction$progressions <- prediction$progressions %>% mutate(percentage = ifelse(percentage > 0 & percentage < 1, runif(n()), percentage))
 
-  results <- calculate_branch_overlap(dataset, prediction)
+
+  results <- calculate_mapping_branches(dataset, prediction)
   testthat::expect_true(all(
     results$recovery_branches == 1,
     results$relevance_branches == 1,
