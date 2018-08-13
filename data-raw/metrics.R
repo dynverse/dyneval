20091912af9f07b2813b7f14575864287f6476c8
zouter <wouter.saelens@gmail.com>
2018-08-13 08:58:33 +0200

    update metric categories

--- data-raw/metrics.R
+++ data-raw/metrics.R
@@ -10,9 +10,9 @@ metrics <- tribble(
   "lm_mse", "MSE[lm]", "\\mathit{MSE}_{lm}", "Linear regression MSE", "neighbourhood", 0, 0.3,
   "lm_rsq", "R[lm]^2", "R^{2}_{lm}", "Linear regression R²", "neighbourhood", 1, 0,
   "edge_flip", "edgeflip", "\\textrm{edgeflip}", "Edge flip", "topology", 1, 0,
-  "featureimp_cor", "cor[features]", "\\mathit{cor}_{\\textrm{features}}", "Feature importance correlation", "Feature importance", 1, 0,
-  "F1_branches", "F1[branches]", "\\mathit{F1}_{\\textit{branches}}", "Overlap between the branches", "mapping", 1, 0,
-  "F1_milestones", "F1[milestones]", "\\mathit{F1}_{\\textit{milestones}}", "Overlap between the milestones", "mapping", 1, 0,
+  "featureimp_cor", "cor[features]", "\\mathit{cor}_{\\textrm{features}}", "Feature importance correlation", "features", 1, 0,
+  "F1_branches", "F1[branches]", "\\mathit{F1}_{\\textit{branches}}", "Overlap between the branches", "branches", 1, 0,
+  "F1_milestones", "F1[milestones]", "\\mathit{F1}_{\\textit{milestones}}", "Overlap between the milestones", "branches", 1, 0,
   "harm_mean", "h-mean", "\\textrm{h-mean}", "Harmonic mean", "Average", 1, 0
 )
 
